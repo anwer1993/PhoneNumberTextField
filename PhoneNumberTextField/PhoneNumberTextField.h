21dfc820f68f328df191de7ef8438a3d2d08ba21
Anwar Hajji <anwarhajji@Mac-Ouss.local>
2022-08-22 17:20:15 +0100

    Initial project setup

---
+++ PhoneNumberTextField/PhoneNumberTextField.h
@@ -0,0 +1,18 @@
+//
+//  PhoneNumberTextField.h
+//  PhoneNumberTextField
+//
+//  Created by Anwar Hajji on 22/08/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PhoneNumberTextField.
+FOUNDATION_EXPORT double PhoneNumberTextFieldVersionNumber;
+
+//! Project version string for PhoneNumberTextField.
+FOUNDATION_EXPORT const unsigned char PhoneNumberTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhoneNumberTextField/PublicHeader.h>
+
+
---
+++ PhoneNumberTextFieldDemo/PhoneNumberTextFieldDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneNumberTextFieldDemoApp.swift
+//  PhoneNumberTextFieldDemo
+//
+//  Created by Anwar Hajji on 22/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct PhoneNumberTextFieldDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
